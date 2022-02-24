93d386e40de70563efb6e808abfd24aad5494557
lipeeecz <89159381+lipeeecz@users.noreply.github.com>
2022-02-23 22:05:19 -0300

    Commit

---
+++ ex02.js
@@ -0,0 +1,4 @@
+var quant = parseFloat(prompt("Digite a quantidade a ser convertida"))
+
+alert("A convers찾o desta quantidade de real para d처lar fica: " + (quant / 5.43))
+alert("A convers찾o desta quantidade de d처lar para real fica: " + (quant * 5.43))
